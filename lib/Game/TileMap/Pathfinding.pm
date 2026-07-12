bd863992193659ebd5cfec2523aec90eb708888e
bbrtj <bbrtj.pro@gmail.com>
2026-07-12 20:33:47 +0200

    Initial - mint dist

---
+++ lib/Game/TileMap/Pathfinding.pm
@@ -0,0 +1,41 @@
+package Game::TileMap::Pathfinding;
+
+use v5.14;
+use warnings;
+
+require XSLoader;
+XSLoader::load('Game::TileMap::Pathfinding', $Game::TileMap::Pathfinding::VERSION);
+
+1;
+
+__END__
+
+=head1 NAME
+
+Game::TileMap::Pathfinding - New module
+
+=head1 SYNOPSIS
+
+	use Game::TileMap::Pathfinding;
+
+	# do something
+
+=head1 DESCRIPTION
+
+This module lets you blah blah blah.
+
+=head1 SEE ALSO
+
+L<Some::Module>
+
+=head1 AUTHOR
+
+Bartosz Jarzyna E<lt>bbrtj.pro@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2026 by Bartosz Jarzyna
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
